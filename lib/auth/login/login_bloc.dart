367aefbcf002b9479cc77dbbf0dde982e84066c2
Karthikeyan Gopal <karthikeyangopal@Karthikeyans-Mac-mini.local>
2022-03-02 10:01:34 +0530

    2-3-22

--- lib/auth/login/login_bloc.dart
+++ lib/auth/login/login_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loginwithbloc/auth/auth_repository.dart';
 import 'package:loginwithbloc/auth/form_submission_status.dart';
@@ -6,47 +8,52 @@ import 'package:loginwithbloc/auth/login/login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   final AuthRepository authRepo;
-  LoginBloc(this.authRepo) : super(LoginState(username: '', password: ''));
-  //
-  //
+  LoginBloc(this.authRepo) : super(LoginState(username: '', password: ''))
+  {
+    // TODO: No need registration of this bloc in Old Bloc
+    on<LoginPasswordChanged>(_onPasswordChanged);
+    on<LoginUsernameChanged>(_onUsernameChanged);
+    on<LoginSubmitted>(_onSubmitted);
+
+  }
+  // TODO: Old Bloc Code
   // @override
-  // Stream<void> onEvent(LoginEvent event) async* {
+  // Stream<LoginState> mapEventToState(LoginEvent event) async* {
   //   if (event is LoginUsernameChanged) {
-  //     // yield state.copyWith(username: event.username);
+  //     print(event.username);
+  //     yield state.copyWith(username: event.username);
   //   } else if (event is LoginPasswordChanged) {
-  //     // yield state.copyWith(password: event.password);
+  //     print(event.password);
+  //     yield state.copyWith(password: event.password);
   //   } else if (event is LoginSubmitted) {
   //     yield state.copyWith(formstatus: FormSubmitting());
   //     try {
   //       await authRepo.login();
   //       yield state.copyWith(formstatus: SubmitSuccess());
   //     } on Exception catch (e) {
-  //         yield state.copyWith(formstatus: SubmitFailed(e));
+  //       print("dsfdsfds");
+  //       yield state.copyWith(formstatus: SubmitFailed(e));
   //     }
   //   }
-  //   // TODO: implement onEvent
-  //   super.onEvent(event);
+  //
+  //   super.mapEventToState(event);
   // }
 
-  @override
-  Stream<LoginState> mapEventToState(LoginEvent event) async* {
-    if (event is LoginUsernameChanged) {
-      print(event.username);
-      yield state.copyWith(username: event.username);
-    } else if (event is LoginPasswordChanged) {
-      print(event.password);
-      yield state.copyWith(password: event.password);
-    } else if (event is LoginSubmitted) {
-      yield state.copyWith(formstatus: FormSubmitting());
-      try {
-        await authRepo.login();
-        yield state.copyWith(formstatus: SubmitSuccess());
-      } on Exception catch (e) {
-        print("dsfdsfds");
-        yield state.copyWith(formstatus: SubmitFailed(e));
-      }
-    }
-    // TODO: implement mapEventToState
-    super.mapEventToState(event);
+  FutureOr<void> _onPasswordChanged(LoginPasswordChanged event, Emitter<LoginState> emit) {
+    emit(state.copyWith(password: event.password));
+  }
+
+  FutureOr<void> _onUsernameChanged(LoginUsernameChanged event, Emitter<LoginState> emit) {
+    emit(state.copyWith(username: event.username));
+  }
+
+  Future<FutureOr<void>> _onSubmitted(LoginSubmitted event, Emitter<LoginState> emit) async {
+    emit(state.copyWith(formstatus: FormSubmitting()));
+        try {
+          await authRepo.login();
+          emit(state.copyWith(formstatus: SubmitSuccess()));
+        } on Exception catch (e) {
+          emit( state.copyWith(formstatus: SubmitFailed(e)));
+        }
   }
 }
